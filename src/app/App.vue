0d9696c69b9b5f621b84949f17ee7cf40c28268f
EVER-ON <gurkadima2004@bk.ru>
2024-10-26 07:29:03 +0300

    refactor: setup for router

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import LoginRegisterPage from "../components/LoginRegisterComponent.vue";
 import Header from "../components/Header.vue";
+import {computed} from 'vue';
+import {useRoute} from 'vue-router';
+
 </script>
 
 <template>
   <Header />
+<main>
+  <router-view />
+</main>
 
-  <div class="m-10 flex items-center justify-center">
-    <LoginRegisterPage />
-  </div>
 
 </template>
