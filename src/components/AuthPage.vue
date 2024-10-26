3c874c74f252bf9429810029d7eaa88a1033ee5f
EVER-ON <gurkadima2004@bk.ru>
2024-10-26 07:27:17 +0300

    feat: create MainPage, AuthPage, NotFound

---
+++ src/components/AuthPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import LoginRegisterPage from "@/components/LoginRegisterComponent.vue";
+</script>
+
+<template>
+  <div class="m-10 flex items-center justify-center">
+    <LoginRegisterPage />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
