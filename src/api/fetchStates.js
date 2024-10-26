d17ca1564827d78d55b8fa17317d5bc5ae1cfe5e
EVER-ON <gurkadima2004@bk.ru>
2024-10-26 09:27:13 +0300

    feat: create method for get stages

---
+++ src/api/fetchStates.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:80/';
+const apiClient = axios.create({
+    baseURL: API_URL,
+});
+
+export async function loadStates() {
+    try {
+        const response = await apiClient.get('api/states', {
+            headers: {
+                authorization: `Bearer ${localStorage.access_token}`,
+            },
+        });
+
+        const states = response.data;
+        console.log(states); // Проверка полученных данных
+        return states;
+    } catch (error) {
+        console.error("Error loading states:", error);
+        throw error;
+    }
+}
---
+++ src/components/StageColumn.vue
@@ -0,0 +1,34 @@
+<template>
+  <Card class="flex h-5/6 border-2 border-solid drop-shadow-2xl vw ">
+    <template #title>{{ title }}</template>
+    <template #content>
+      <p class="m-0">
+
+      </p>
+    </template>
+  </Card>
+
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: false
+  },
+  stageNumber: {
+    type: Number,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
