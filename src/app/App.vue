e15d25d5a1b5fc217d63bfe747e2c9a43cb951c7
Radon24 <radon24code@gmail.com>
2024-10-26 05:51:14 +0300

    refactor: moved to /app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-import LoginRegisterPage from "./components/loginRegisterComponent.vue";
+import LoginRegisterPage from "../components/LoginRegisterComponent.vue";
+import Header from "../components/Header.vue";
 </script>
 
 <template>
+  <Header />
+
   <div class="m-10 flex items-center justify-center">
     <LoginRegisterPage />
   </div>
